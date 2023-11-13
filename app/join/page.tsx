acc00553e8e40e1fe55ff57c310498cdf2889883
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2023-11-13 10:30:07 +0800

    fix: close job (#25)

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -43,20 +43,31 @@ function Page() {
         {joinPageConfig.currentJobTitle}
       </div>
       <div className={'jobs'}>
-        {joinPageConfig.jobs.map((item) => (
-          <Link target={'_blank'} href={item.link} key={item.title} className={'job'}>
-            <div className={'job-title'}>{item.title}</div>
-            <div className={'job-link-icon'}>
-              <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox='0 0 32 33' fill='none'>
-                <path
-                  d='M3.83984 4.33984H28.1598M28.1598 4.33984V28.6598M28.1598 4.33984L3.83984 28.6598'
-                  stroke='currentColor'
-                  strokeWidth='3'
-                />
-              </svg>
-            </div>
-          </Link>
-        ))}
+        {joinPageConfig.jobs.length > 0 ? (
+          joinPageConfig.jobs.map((item: { title: string; link: string }) => (
+            <Link target={'_blank'} href={item.link} key={item.title} className={'job'}>
+              <div className={'job-title'}>{item.title}</div>
+              <div className={'job-link-icon'}>
+                <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' viewBox='0 0 32 33' fill='none'>
+                  <path
+                    d='M3.83984 4.33984H28.1598M28.1598 4.33984V28.6598M28.1598 4.33984L3.83984 28.6598'
+                    stroke='currentColor'
+                    strokeWidth='3'
+                  />
+                </svg>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <div className={'no-job flex flex-col items-center justify-center'}>
+            <div
+              className={'mb-4 text-xl font-bold'}
+            >{`We're not hiring at the moment, but please check back later.`}</div>
+            <div
+              className={'text'}
+            >{`In the meantime, you're welcome to contribute to our open source repository and collaborate with us there!`}</div>
+          </div>
+        )}
       </div>
     </div>
   );
