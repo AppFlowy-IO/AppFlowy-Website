6108c83e0e8bb57b16105e4ae5356954fecf5e79
qinluhe <qinluhe.twodog@gmail.com>
2024-01-09 15:50:02 +0800

    feat: support ios download

---
+++ components/icons/test-flight.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestFlight() {
+  return (
+    <div className={'h-[22px] w-[22px] items-center justify-center'}>
+      <img src={'/images/test-flight.png'} alt={'TestFlight'} />
+    </div>
+  );
+}
+
+export default TestFlight;
