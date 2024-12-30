438f1db9c8eb14ab3cff59c6abdb253d9a51a663
Kilu <lu@appflowy.io>
2024-12-30 16:47:51 +0800

    fix: modified universal link path

--- app/.well-known/apple-app-site-association/route.ts
+++ app/.well-known/apple-app-site-association/route.ts
@@ -1,34 +1,27 @@
-
 import { NextResponse } from 'next/server';
 
 const body = {
-  "applinks": {
-    "apps": [],
-    "details": [
+  applinks: {
+    apps: [],
+    details: [
       {
-        "appIDs": [
-          "VHB67HRSZG.com.appflowy.appflowy.flutter"
-        ],
-        "paths": [
-          "*"
-        ],
-        "components": [
+        appIDs: ['VHB67HRSZG.com.appflowy.appflowy.flutter'],
+        paths: ['/download', '/download/*'],
+        components: [
           {
-            "/": "/*"
-          }
-        ]
-      }
-    ]
+            '/': '/*',
+          },
+        ],
+      },
+    ],
   },
-  "webcredentials": {
-    "apps": [
-      "VHB67HRSZG.com.appflowy.appflowy.flutter"
-    ]
-  }
-}
+  webcredentials: {
+    apps: ['VHB67HRSZG.com.appflowy.appflowy.flutter'],
+  },
+};
+
 export async function GET() {
-  return new NextResponse(JSON.stringify(body),
-    {
+  return new NextResponse(JSON.stringify(body), {
     headers: {
       'Content-Type': 'application/json',
     },
