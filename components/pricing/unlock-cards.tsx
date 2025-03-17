d62a4fdbaae008a9526d7b4c42599f95c851f7b6
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2025-03-17 17:36:16 +0800

    fix: update pricing (#102)

--- components/pricing/unlock-cards.tsx
+++ components/pricing/unlock-cards.tsx
@@ -5,7 +5,6 @@ import { Tick } from '@/components/pricing/icons';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useCallback } from 'react';
-import AI2 from '@/assets/images/pricing/ai-2.png';
 
 function UnlockCards() {
   const renderCard = useCallback(
@@ -56,27 +55,10 @@ function UnlockCards() {
           height={90}
         />,
         title: 'AI MAX',
-        desc: 'Access the most advanced AI models including GPT 4-o and Claude 3 Sonnet',
+        desc: 'Access the most advanced AI models including GPT-4o, GPT-o3-mini, DeepSeek R1, and Claude 3.5 Sonnet',
         num: '$8',
         duration: `per user per month\nbilled annually`,
         content: ['Unlimited AI responses', 'Unlimited file uploads', '50 AI images per month'],
-      })}
-      {renderCard({
-        icon: <Image
-          src={AI2}
-          alt={''}
-          width={74.5}
-          height={90}
-        />,
-        title: 'AI On-device for Mac',
-        desc: 'Run Mistral 7B, Llama 8B, and more local models on your machine',
-        num: '$8',
-        duration: `per user per month\nbilled annually`,
-        content: [
-          'Local AI on your own hardware for ultimate privacy',
-          'Unlimited AI responses',
-          'Unlimited file uploads',
-        ],
       })}
     </div>
   );
