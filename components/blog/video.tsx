27b7f4fa6a155554aaa3a1a1538248059b8ad08a
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2025-08-04 11:36:05 +0800

    chore: add blogs on 07-31 (#116)

--- components/blog/video.tsx
+++ components/blog/video.tsx
@@ -10,16 +10,25 @@ interface Props {
 function Video(props: Props) {
   const { src, caption, alt } = props;
 
+  const isLocalVideo = src.match(/\.(mp4|webm|ogg|mov|avi|mkv)$/i);
+
   return (
     <>
-      <div className='video-container'>
-        <iframe
-          className='w-full'
-          src={src}
-          title={alt}
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share'
-          allowFullScreen
-        />
+      <div className='video-container' style={{ paddingBottom: !isLocalVideo ? '56.25%' : '0' }}>
+        {isLocalVideo ? (
+          <video autoPlay={false} className='w-full rounded-md' controls preload='metadata' title={alt}>
+            <source src={src} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <iframe
+            className='aspect-video w-full rounded-md'
+            src={src}
+            title={alt}
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share'
+            allowFullScreen
+          />
+        )}
       </div>
       {caption && <Caption content={caption} />}
     </>
