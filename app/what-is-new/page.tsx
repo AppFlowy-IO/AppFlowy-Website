2b3b4f35ad9371b5f0c1664960df69a9c20cb806
MayurSMahajan <mayur23smahajan@gmail.com>
2026-08-03 11:55:54 +0530

    feat: whats' new page complete

--- app/what-is-new/page.tsx
+++ app/what-is-new/page.tsx
@@ -3,6 +3,7 @@ import { fetchVersions } from '@/lib/githubAPI';
 import { whatIsNewConfig } from '@/lib/config/pages';
 import Versions from '@/components/what-is-new/versions';
 import { parseChangelog } from '@/lib/parseChangelog';
+import GetStart from '@/components/product/get-start';
 import { Metadata } from 'next';
 
 const site_url = process.env.NEXT_PUBLIC_SITE_BASE_URL;
@@ -20,33 +21,17 @@ async function Page() {
 
   return (
     <div className="what-is-new-page">
-      <div className={'title'}>
-        {whatIsNewConfig.title}
-        <div className={'line'}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
-            viewBox="0 0 198 19"
-            fill="none"
-          >
-            <path
-              d="M196.216 14.5974C142.824 7.17755 105.479 -2.82286 17.9105 4.9016C15.5675 5.10828 15.4591 8.40096 17.7864 8.74184L74.3253 17.0231C39.8213 10.0151 20.9741 9.90925 2.19985 13.4713"
-              stroke="#9327FF"
-              strokeWidth="3"
-              strokeLinecap="square"
-            />
-          </svg>
-        </div>
-      </div>
+      <div className="z-10 px-6 text-center text-style-h1 font-bold">{whatIsNewConfig.title}</div>
+      <div className={'subtitle'}>{whatIsNewConfig.subtitle}</div>
       <div className={'content'}>
         <Versions versions={versions.map(parseChangelog)} />
       </div>
+      <GetStart />
     </div>
   );
 }
 
-const getData = async() => {
+const getData = async () => {
   try {
     return await fetchVersions();
   } catch {
--- components/what-is-new/versions.tsx
+++ components/what-is-new/versions.tsx
@@ -1,36 +1,16 @@
-import React, { useMemo } from 'react';
-import VersionGroup from '@/components/icons/version-group';
+import React from 'react';
 import VersionComponent from '@/components/what-is-new/version';
 import { IVersion } from '@/lib/config/versions';
-import dayjs from 'dayjs';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat';
-import { getVersions } from '@/lib/versions';
-
-dayjs.extend(LocalizedFormat);
 
 function Versions({ versions }: { versions: IVersion[] }) {
-  const monthVersions: (IVersion & {
-    month?: string;
-  })[] = useMemo(() => getVersions(versions), [versions]);
-
   return (
     <div className={'versions'}>
-      {monthVersions.map((version) => {
-        return (
-          <div className={'month-panel'} key={version.version}>
-            {version.month ? (
-              <div className={'month'}>
-                <i className={'text-primary'}>
-                  <VersionGroup />
-                </i>
-                {version.month}
-              </div>
-            ) : null}
-
-            <VersionComponent version={version} key={version.version} />
-          </div>
-        );
-      })}
+      {versions.map((version, index) => (
+        <React.Fragment key={version.version}>
+          {index > 0 && <div className={'version-divider'} />}
+          <VersionComponent version={version} />
+        </React.Fragment>
+      ))}
     </div>
   );
 }
