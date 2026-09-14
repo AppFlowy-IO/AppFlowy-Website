a58ea36383e1bb571b1b713130c04475d7c5983c
MayurSMahajan <mayur23smahajan@gmail.com>
2026-09-15 00:27:09 +0530

    feat: download page hero section ready

--- components/download/os.tsx
+++ components/download/os.tsx
@@ -3,31 +3,20 @@ import React from 'react';
 import { downloadPageConfig } from '@/lib/config/pages';
 import DownloadOsBtn from '@/components/download/os-btn';
 import Image from 'next/image';
-import image0 from '@/assets/images/download/img-0.png';
-import darkImage0 from '@/assets/images/download/dark/img-0.png';
+import DownloadIllustration from '@/assets/images/download/download-illustration.webp';
 
 function DownloadOS() {
   return (
     <>
       <DownloadOsBtn />
       <div className={'download-os image relative z-[1] aspect-video h-fit w-full max-w-screen-xl'}>
-        <div className={'ellipse'} />
         <div className={'absolute-image aspect-auto h-fit'}>
           <Image
-            className={'relative mx-[20px]'}
-            src={image0.src}
+            className={'relative mx-[28px]'}
+            src={DownloadIllustration}
             alt={downloadPageConfig.downloadOSImageAlt}
-            width={1162}
-            height={613.8}
-          />
-        </div>
-        <div className={'absolute-image dark-image aspect-auto h-fit'}>
-          <Image
-            className={'relative mx-[20px]'}
-            src={darkImage0.src}
-            alt={downloadPageConfig.downloadOSImageAlt}
-            width={1162}
-            height={613.8}
+            width={1280}
+            height={740}
           />
         </div>
       </div>
